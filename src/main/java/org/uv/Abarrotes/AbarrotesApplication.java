c1a5e87452a77f0003c05d966189cef6acb477df
Loken <rodrigomencias08@gmail.com>
2023-11-07 17:24:37 -0600

    creación del proyecto

---
+++ src/main/java/org/uv/Abarrotes/AbarrotesApplication.java
@@ -0,0 +1,13 @@
+package org.uv.Abarrotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbarrotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbarrotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/uv/Abarrotes/AbarrotesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.Abarrotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbarrotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
