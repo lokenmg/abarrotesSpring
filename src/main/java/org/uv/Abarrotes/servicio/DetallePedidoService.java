a4a27d926d873158b54db5d37583d31d370e93ed
Loken <rodrigomencias08@gmail.com>
2023-12-09 19:01:57 -0600

    creación de detallePedidoService

--- src/main/java/org/uv/Abarrotes/servicio/DetallePedidoService.java
+++ src/main/java/org/uv/Abarrotes/servicio/DetallePedidoService.java
@@ -1,5 +1,20 @@
 package org.uv.Abarrotes.servicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.uv.Abarrotes.modelos.DetallePedido;
+import org.uv.Abarrotes.repositorio.DetallePedidoRepository;
+
+import DTOs.DTODetallePedido;
+
+@Service
 public class DetallePedidoService {
+    @Autowired
+    private DetallePedidoRepository detallePedidoRepository;
+
+    public DTODetallePedido crearDetallePedido(DetallePedido detallePedido){
+        return new DTODetallePedido(detallePedidoRepository.save(detallePedido));
+    }
+
     
 }
