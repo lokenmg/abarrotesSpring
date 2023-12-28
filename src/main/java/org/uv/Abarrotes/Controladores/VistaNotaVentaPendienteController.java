b8e6caf27fd4210eea8a08cda88fe37ba23c96aa
YACRUZ <yamatsukino@gmail.com>
2023-12-27 21:26:59 -0600

    Add vistas

---
+++ src/main/java/org/uv/Abarrotes/Controladores/VistaNotaVentaPendienteController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.Abarrotes.Controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.uv.Abarrotes.modelos.VistaNotaVentaPagada;
+
+import java.util.List;
+import org.uv.Abarrotes.modelos.VistaNotaVentaPendiente;
+import org.uv.Abarrotes.servicio.VistaNotaVentaPendienteService;
+/**
+ *
+ * @author yacruz
+ */
+@RestController
+@RequestMapping("/api/vista-nota-venta-pendiente")
+public class VistaNotaVentaPendienteController {
+    @Autowired
+    private VistaNotaVentaPendienteService vistaNotaVentaPendienteService;
+
+    @GetMapping
+    public ResponseEntity<List<VistaNotaVentaPendiente>> getAllVistaNotaVentaPendiente() {
+        return ResponseEntity.ok(vistaNotaVentaPendienteService.getAllVistaNotaVentaPendiente());
+    }
+}
---
+++ src/main/java/org/uv/Abarrotes/modelos/VistaNotaVentaPagada.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.Abarrotes.modelos;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.sql.Date;
+
+
+/**
+ *
+ * @author yacruz
+ */
+@Entity
+@Table(name = "vistanotaventapagada")
+public class VistaNotaVentaPagada {
+    @Id
+    private Long numeroNota;
+
+    private Date fechaAnticipo;
+    private BigDecimal monto;
+    private BigDecimal resto;
+    private String estadoPago;
+    private String nombreCompletoCliente;
+    private String telefonoCliente;
+    private String direccionCliente;
+    private String nombreCompletoEmpleado;
+    private Date fechaNota;
+    private BigDecimal total;
+    private String nombreDepartamento;
+
+    public Long getNumeroNota() {
+        return numeroNota;
+    }
+
+    public void setNumeroNota(Long numeroNota) {
+        this.numeroNota = numeroNota;
+    }
+
+    public Date getFechaAnticipo() {
+        return fechaAnticipo;
+    }
+
+    public void setFechaAnticipo(Date fechaAnticipo) {
+        this.fechaAnticipo = fechaAnticipo;
+    }
+
+    public BigDecimal getMonto() {
+        return monto;
+    }
+
+    public void setMonto(BigDecimal monto) {
+        this.monto = monto;
+    }
+
+    public BigDecimal getResto() {
+        return resto;
+    }
+
+    public void setResto(BigDecimal resto) {
+        this.resto = resto;
+    }
+
+    public String getEstadoPago() {
+        return estadoPago;
+    }
+
+    public void setEstadoPago(String estadoPago) {
+        this.estadoPago = estadoPago;
+    }
+
+    public String getNombreCompletoCliente() {
+        return nombreCompletoCliente;
+    }
+
+    public void setNombreCompletoCliente(String nombreCompletoCliente) {
+        this.nombreCompletoCliente = nombreCompletoCliente;
+    }
+
+    public String getTelefonoCliente() {
+        return telefonoCliente;
+    }
+
+    public void setTelefonoCliente(String telefonoCliente) {
+        this.telefonoCliente = telefonoCliente;
+    }
+
+    public String getDireccionCliente() {
+        return direccionCliente;
+    }
+
+    public void setDireccionCliente(String direccionCliente) {
+        this.direccionCliente = direccionCliente;
+    }
+
+    public String getNombreCompletoEmpleado() {
+        return nombreCompletoEmpleado;
+    }
+
+    public void setNombreCompletoEmpleado(String nombreCompletoEmpleado) {
+        this.nombreCompletoEmpleado = nombreCompletoEmpleado;
+    }
+
+    public Date getFechaNota() {
+        return fechaNota;
+    }
+
+    public void setFechaNota(Date fechaNota) {
+        this.fechaNota = fechaNota;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public String getNombreDepartamento() {
+        return nombreDepartamento;
+    }
+
+    public void setNombreDepartamento(String nombreDepartamento) {
+        this.nombreDepartamento = nombreDepartamento;
+    }
+    
+}
