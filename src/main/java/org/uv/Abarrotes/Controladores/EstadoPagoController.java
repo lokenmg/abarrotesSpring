9544827eea2cbe5169511e219153f4301afe8382
Itzelll <mmitzel2435@gmail.com>
2024-01-02 11:38:03 -0600

    add cors EstadoPago

--- src/main/java/org/uv/Abarrotes/Controladores/EstadoPagoController.java
+++ src/main/java/org/uv/Abarrotes/Controladores/EstadoPagoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.uv.Abarrotes.modelos.EstadoPago;
 import org.uv.Abarrotes.servicio.EstadoPagoService;
 
 @RequestMapping("/api/estadopago")
+@CrossOrigin(origins="*", allowCredentials="")
 @RestController
 public class EstadoPagoController {
     @Autowired
